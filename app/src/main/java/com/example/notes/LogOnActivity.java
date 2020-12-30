58a73cc6f87e3e3a91980214497ba77a5c61576d
Krzysztof <kkignasiak.98@gmail.com>
2020-12-30 13:37:09 +0100

    enable fingerprint authentication

--- app/src/main/java/com/example/notes/LogOnActivity.java
+++ app/src/main/java/com/example/notes/LogOnActivity.java
@@ -1,19 +1,28 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+//import android.hardware.biometrics.BiometricManager;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.concurrent.Executor;
+
 public class LogOnActivity extends AppCompatActivity {
 
     EditText etUsername, etPassword;
     Button btSubmit;
+    Button btFinger;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +49,69 @@ public class LogOnActivity extends AppCompatActivity {
                             "invalid Username or Password",Toast.LENGTH_SHORT).show();
                 }
             }
+
+        });
+
+        btFinger = findViewById(R.id.login_btn);
+
+        BiometricManager biometricManager;
+        biometricManager = BiometricManager.from(this);
+
+        switch(biometricManager.canAuthenticate())
+        {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+               // Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+              //  startActivity(intent);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(),
+                        "the device don't have a fingerprint sensor",Toast.LENGTH_SHORT).show();
+                btFinger.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(getApplicationContext(),
+                        "the biometric sensors in currently unavailable",Toast.LENGTH_SHORT).show();
+                btFinger.setVisibility(View.GONE);
+                break;
+             case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                 Toast.makeText(getApplicationContext(),
+                         "your device don't have any fingerprint saved",Toast.LENGTH_SHORT).show();
+                 break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(LogOnActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                  startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("login")
+                .setDescription("Use your fingerprint to login to your app")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        btFinger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
         });
     }
+
+
 }
