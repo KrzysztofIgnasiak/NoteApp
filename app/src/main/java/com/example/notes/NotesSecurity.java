60dbf072970fba3700cee184eb70187887041133
Krzysztof <kkignasiak.98@gmail.com>
2021-02-08 16:45:32 +0100

    refactor NoteSecurity (unused methods, comments, naming)

--- app/src/main/java/com/example/notes/NotesSecurity.java
+++ app/src/main/java/com/example/notes/NotesSecurity.java
@@ -12,75 +12,42 @@ import java.util.Arrays;
 
 public class NotesSecurity {
 
-    public static ArrayList<String>DecryptNotes(ArrayList<String> EncryptedNotes, Context context, byte [] iv)
+    public static String DecryptNote(String encryptedNote,Context context, byte [] iv)
     {
-        ArrayList<String> Notes = new ArrayList<String>();
-        for (int i = 0;i<EncryptedNotes.size();i++)
-        {
-            String CurrentEncryptedNote = EncryptedNotes.get(i);
+        byte [] currentEncryptedNoteBytes = new byte[0]; // get bytes
+        currentEncryptedNoteBytes = Base64.decode(encryptedNote,Base64.NO_WRAP); // to bytes
+        byte [] currentNoteBytes = KeyStore_subSystem.DecryptData(currentEncryptedNoteBytes,context,iv);// decrypt
 
-           String CurrentNote = DecryptNote(CurrentEncryptedNote,context,iv);
-            Notes.add(CurrentNote);
-        }
-        return Notes;
+        String note = null;
+        note = ConvertToString(currentNoteBytes); // back to string
+        return note;
     }
-    public static ArrayList<String>EncryptNotes(ArrayList<String> Notes, Context context, byte [] iv)
+    public static String EncryptNote(String note, Context context, byte[] iv)
     {
-        ArrayList<String> EncryptedNotes = new ArrayList<String>();
-        for (int i = 0;i<Notes.size();i++)
-        {
-            String CurrentNote = Notes.get(i);
-
-            String CurrentEncryptedNote = EncryptNote(CurrentNote,context,iv);
-            Notes.add(CurrentEncryptedNote);
-        }
-        return Notes;
-    }
-
-    public static String DecryptNote(String EncryptedNote,Context context, byte [] iv)
-    {
-        byte [] CurrentEncryptedNoteBytes = new byte[0]; // get bytes
-        CurrentEncryptedNoteBytes = Base64.decode(EncryptedNote,Base64.NO_WRAP); // to bytes
-        byte [] CurrentNoteBytes = KeyStore_subSystem.DecryptData(CurrentEncryptedNoteBytes,context,iv);// decrypt
-
-        String Note = null;
-        // Note = new String(CurrentNoteBytes,"UTF-8");
-        Note = ConvertToString(CurrentNoteBytes);
-        return Note;
-    }
-    public static String EncryptNote(String Note, Context context, byte[] iv)
-    {
-        byte [] NoteBytes = new byte[0];
+        byte [] noteBytes = new byte[0];
         try {
-            NoteBytes = Note.getBytes("UTF-8"); // to bytes
+            noteBytes = note.getBytes("UTF-8"); // to bytes
         } catch (UnsupportedEncodingException e) {
 
             e.printStackTrace();
         }
-        EncryptHandler Handler = KeyStore_subSystem.EncryptData2(context,NoteBytes,iv);
-        byte [] NoteEncryptedBytes = Handler.getEncrypted();
-        String NoteEncrypted =  Base64.encodeToString(NoteEncryptedBytes, Base64.NO_WRAP);
-       // String NoteEncrypted = ConvertToString(NoteEncryptedBytes);
-        return NoteEncrypted;
+        EncryptHandler handler = KeyStore_subSystem.EncryptData2(context,noteBytes,iv); //encrypt
+        byte [] noteEncryptedBytes = handler.getEncrypted(); //get encrypted bytes
+
+        String noteEncrypted =  Base64.encodeToString(noteEncryptedBytes, Base64.NO_WRAP); //convert to string
+        return noteEncrypted;
 
     }
-    public static ArrayList<String> UpdateEncryptedNotes(ArrayList<String> EncryptedNotes,byte [] iv, int index,Context context)
-    {
-        String NoteToUpdate = EncryptedNotes.get(index);
-        String UpdatedNote = EncryptNote(NoteToUpdate,context,iv);
-        EncryptedNotes.set(index, NoteToUpdate);
 
-        return EncryptedNotes;
-    }
     public static String ConvertToString(byte[] chars)
     {
         StringBuilder result = new StringBuilder();
         String temp = "";
         for(int i =0;i<chars.length;i++)
         {
-            byte[] tempbyte = Arrays.copyOfRange(chars,i,i+1);
+            byte[] bytes = Arrays.copyOfRange(chars,i,i+1);
             try {
-                temp = new String(tempbyte,"UTF-8");
+                temp = new String(bytes,"UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
