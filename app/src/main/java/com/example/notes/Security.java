fff8a105faf054964d754d445ba841b31bd1350c
Krzysztof <kkignasiak.98@gmail.com>
2021-02-08 16:53:17 +0100

    refactor Security.java

--- app/src/main/java/com/example/notes/Security.java
+++ app/src/main/java/com/example/notes/Security.java
@@ -15,17 +15,17 @@ import javax.crypto.spec.PBEKeySpec;
 public class Security {
 
     private static final int INTERATIONS = 65536; // how many times we should perform the hashing algorithm
-    private static final int KEY_LENGHT = 512; // the desired length of the key in bites
+    private static final int KEY_LENGTH = 512; // the desired length of the key in bites
     private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
 
-    private static final SecureRandom Rand = new SecureRandom();
+    private static final SecureRandom rand = new SecureRandom();
 
     public static Optional<String> hashPassword(String password, String salt)
     {
         char[] chars = password.toCharArray();
         byte[] bytes = salt.getBytes();
 
-        PBEKeySpec spec = new PBEKeySpec(chars,bytes, INTERATIONS,KEY_LENGHT); // how we'are going to generated hashed password
+        PBEKeySpec spec = new PBEKeySpec(chars,bytes, INTERATIONS, KEY_LENGTH); // how we'are going to generated hashed password
 
         Arrays.fill(chars, Character.MIN_VALUE); // all information is in spec so it is good idea to delete the oryginal password
 
@@ -50,11 +50,11 @@ public class Security {
     {
         if(length<1)
         {
-            System.err.println("error in generateSalt : lenth must be >0");
+            System.err.println("error in generateSalt : length must be >0");
         }
 
         byte[] salt = new byte[length];
-        Rand.nextBytes(salt);
+        rand.nextBytes(salt);
 
         return Optional.of(Base64.getEncoder().encodeToString(salt));
     }
