4c0b5243756835fd773050091c0d41afa6771e0b
Krzysztof <kkignasiak.98@gmail.com>
2021-02-08 17:34:42 +0100

    delete unused imports in LogOnActivity.java

--- app/src/main/java/com/example/notes/LogOnActivity.java
+++ app/src/main/java/com/example/notes/LogOnActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-//import android.hardware.biometrics.BiometricManager;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
